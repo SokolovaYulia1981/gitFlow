d121e8ba4efbe422721b44980e8c272a04745ed9
SokolovaYulia1981 <149901307+SokolovaYulia1981@users.noreply.github.com>
2023-11-05 15:58:55 +0300

    Delete str_func.py

--- str_func.py
+++
@@ -1,8 +0,0 @@
-def foo(some_str):
-    """Все буквы становятся вверхнем регистре"""
-    return some_str.upper()
-def capitalize_words(string):
-    """делает заглавными первые буквы каждого слова в строчке!"""
-    words = string.split()
-    capitalized_words = [word.capitalize() for word in words]
-    return ' '.join(capitalized_words)
