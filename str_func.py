af044cb23c953ab4a4e124c93da45c84ce205cf8
Makc <kondrashov-max@mail.ru>
2023-11-05 15:24:35 +0300

    my update

--- str_func.py
+++ str_func.py
@@ -1,5 +1,8 @@
 def foo(some_str):
-    """upper all word"""
+    """Все буквы становятся вверхнем регистре"""
     return some_str.upper()
-
-"""Сделай вторую функцию"""
+def capitalize_words(string):
+    """делает заглавными первые буквы каждого слова в строчке"""
+    words = string.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return ' '.join(capitalized_words)
