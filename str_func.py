b259df2bf85c9bbfe909929f49062efe6eceb4a0
Makc <kondrashov-max@mail.ru>
2023-11-05 16:02:01 +0300

    commit again v2

--- str_func.py
+++ str_func.py
@@ -8,3 +8,5 @@ def capitalize_words(string):
     words = string.split()
     capitalized_words = [word.capitalize() for word in words]
     return ' '.join(capitalized_words)
+
+"""Изменений никаких нет"""
